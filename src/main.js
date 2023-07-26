cd1200362a3c6decba768baf4b57322e069dc467
salsabeel noor <salsabeelnoor@gmail.com>
2023-07-26 13:41:22 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
+import store from './store/index'
+import BaseBadge from './components/ui/BaseBadge.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.component('base-badge', BaseBadge)
+app.mount('#app');
